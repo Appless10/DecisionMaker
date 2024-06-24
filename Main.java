4b58a108fad2218a9fdab848c7d5a82286d2d7bd
SageSpikes <94022547+Appless10@users.noreply.github.com>
2024-06-24 10:31:11 -0400

    Update Main.java
    Finished program

--- Main.java
+++ Main.java
@@ -1,21 +1,24 @@
 import java.util.Scanner;
+import java.util.Random;
 public class Main {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
+        Random rand = new Random();
 
         System.out.println("Hello and welcome to the decision maker");
         System.out.println("Please enter how many options you have as an integer");
         int optionInput = input.nextInt();
+        input.nextLine();
 
         String[] optionArray = new String[optionInput];
 
         for(int i = 0; i < optionInput; i++){
             optionArray[i] = input.nextLine();
         }
+        
+        int randElement = rand.nextInt(optionArray.length);
 
-        for(int i = 0; i < optionInput; i++){
-           System.out.println(optionArray[i]);
-        }
+        System.out.print(optionArray[randElement]);
 
     }
 }
